eb4cb6cc93d16baf50ecc13b7505b294044b3a19
GlennOdoom <148824030+GlennOdoom@users.noreply.github.com>
2025-05-11 20:01:26 +0000

    proof

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./Styles/index.css";
+import "./Styles/Index.css";
 //import { AuthContext } from './Contexts/AuthenticationContext'
 import { AuthProvider } from "./Contexts/AuthenticationContext";
 
